554aa6df017dafe0aba217d90ac4cbe23ec0a804
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-07-30 16:27:34 -0300

    CustomErrorStateMatcher do Angular Material

--- resumo-angular-material/src/app/app.module.ts
+++ resumo-angular-material/src/app/app.module.ts
@@ -9,16 +9,18 @@ import { Ex1Component } from './components/ex1/ex1.component';
 
 // Angular material
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { Ex2Component } from './components/ex2/ex2.component';
 import { EmailValidatorDirective } from './components/ex2/directives/email-validator.directive';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { CustomErrorStateMatcher } from './components/ex2/utils/custom-error-sstate-matcher';
 
 @NgModule({
   declarations: [
     AppComponent,
     Ex1Component,
     Ex2Component,
-    
+
     EmailValidatorDirective
   ],
   imports: [
@@ -31,6 +33,10 @@ import { EmailValidatorDirective } from './components/ex2/directives/email-valid
     MatInputModule
   ],
   providers: [
+    {
+      provide: ErrorStateMatcher,
+      useClass: CustomErrorStateMatcher,
+    },
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
--- resumo-angular-material/src/app/components/ex2/ex2.component.ts
+++ resumo-angular-material/src/app/components/ex2/ex2.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { CustomErrorStateMatcher } from './utils/custom-error-sstate-matcher';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 @Component({
   selector: 'app-ex2',
   templateUrl: './ex2.component.html',
-  styleUrl: './ex2.component.css'
+  styleUrl: './ex2.component.css',
+  // providers: [
+  //   {
+  //     provide: ErrorStateMatcher,
+  //     useClass: CustomErrorStateMatcher,
+  //   }
+  // ]
 })
 export class Ex2Component {
-
+  matcher = new CustomErrorStateMatcher();
 }
