52db262e6f0a4927a16a467350f4664ce6ececa2
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-07-30 17:46:09 -0300

    Criação dos modulos

--- projeto-reactive-forms/src/app/app.module.ts
+++ projeto-reactive-forms/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +14,8 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularMaterialModule,
+    ComponentsModule
   ],
   providers: [
     provideAnimationsAsync()
